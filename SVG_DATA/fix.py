68eadc311178c1efb27b833205c063b5a607c30c
Seif eddine <61545317+saifKader@users.noreply.github.com>
2023-07-19 21:29:29 +0100

    model works on single characters

--- SVG_DATA/fix.py
+++ SVG_DATA/fix.py
@@ -1,99 +1,40 @@
-import os
-import random
-from PIL import Image, ImageEnhance, ImageOps
-from skimage.filters import threshold_otsu
-import numpy as np
+import Augmentor
 
+# Define your pipeline
+p = Augmentor.Pipeline("/Users/abdelkaderseifeddine/Documents/GitHub/CMC-7-trained-model/SVG_DATA/photos")
 
-def glitch(image, glitch_factor):
-    np_img = np.array(image.convert("L"))  # Convert image to grayscale
+# Add operations to the pipeline
+# Random rotation between -25 and +25 degrees
+p.rotate(probability=0.7, max_left_rotation=25, max_right_rotation=25)
 
-    # Binarize the image using Otsu's thresholding
-    threshold = threshold_otsu(np_img)
-    np_img = np_img > threshold
+# Randomly flip some of the images left/right
+p.flip_left_right(probability=0.5)
 
-    # Define the maximum shift in pixels for each direction
-    max_x = int(glitch_factor * image.width)
-    max_y = int(glitch_factor * image.height)
+# Randomly flip some of the images top/bottom
+p.flip_top_bottom(probability=0.5)
 
-    if max_x <= 0 or max_y <= 0:
-        return image
+# Randomly crop images
+p.crop_random(probability=0.5, percentage_area=0.8)
 
-    # Create random shifts for each pixel
-    x_shifts = np.random.randint(-max_x, max_x, size=np_img.shape[:2])
-    y_shifts = np.random.randint(-max_y, max_y, size=np_img.shape[:2])
+# Randomly zoom into images
+p.zoom_random(probability=0.5, percentage_area=0.8)
 
-    # Create a new array for the glitched image
-    glitched_img = np.zeros_like(np_img)
+# Randomly distort the images
+p.random_distortion(probability=0.5, grid_width=4, grid_height=4, magnitude=8)
 
-    # Apply the pixel shifts to the new image
-    for x in range(np_img.shape[1]):
-        for y in range(np_img.shape[0]):
-            shift_x = x_shifts[y, x]
-            shift_y = y_shifts[y, x]
+# Randomly skew the images
+p.skew(probability=0.5, magnitude=0.6)
 
-            new_x = (x + shift_x) % np_img.shape[1]
-            new_y = (y + shift_y) % np_img.shape[0]
+# Change the image contrast, saturation, and brightness
+p.random_contrast(probability=0.5, min_factor=0.8, max_factor=1.2)
+p.random_color(probability=0.5, min_factor=0.8, max_factor=1.2)
+p.random_brightness(probability=0.5, min_factor=0.8, max_factor=1.2)
 
-            glitched_img[new_y, new_x] = np_img[y, x]
+# Convert the images to grayscale
+p.black_and_white(probability=0.2)
 
-    # Convert boolean array to int array, and then to an image
-    glitched_img = Image.fromarray(glitched_img.astype(np.uint8) * 255)
+# Resize all images
+p.resize(probability=1, width=64, height=64)
 
-    return glitched_img
-
-
-def rotate_and_sharpen_image(image, angle):
-    rotated_image = image.rotate(angle, Image.BICUBIC, expand=True)
-    enhancer = ImageEnhance.Sharpness(rotated_image)
-    sharpened_image = enhancer.enhance(2.0)
-    return sharpened_image
-
-
-def resize_image(image, image_size):
-    aspect = image.size[0] / image.size[1]
-
-    if aspect > 1:
-        new_width = image_size[0]
-        new_height = int(image_size[0] / aspect)
-    else:
-        new_height = image_size[1]
-        new_width = int(image_size[1] * aspect)
-
-    img = image.resize((new_width, new_height))
-    new_img = Image.new("RGBA", image_size, (0, 0, 0, 0))
-    new_img.paste(img, ((image_size[0] - new_width) // 2, (image_size[1] - new_height) // 2))
-
-    enhancer = ImageEnhance.Sharpness(new_img)
-    new_img = enhancer.enhance(2.0)
-    return new_img
-
-
-train_path = "/Users/abdelkaderseifeddine/Documents/GitHub/CMC-7-trained-model/SVG_DATA/photos"
-image_size = (64, 64)
-angles = list(range(0, 361, 15))
-glitch_levels = [i * 0.01 for i in range(4)]  # 0 to 0.03 in steps of 0.01
-
-if os.path.isdir(train_path):
-    for img_name in os.listdir(train_path):
-        if img_name.endswith('.jpg') or img_name.endswith('.png'):
-            img_path = os.path.join(train_path, img_name)
-            print(f'Processing image: {img_path}')
-            original_img = Image.open(img_path)
-
-            if original_img.mode != 'RGBA':
-                original_img = original_img.convert('RGBA')
-
-            for i, angle in enumerate(angles):
-                for j, glitch_factor in enumerate(glitch_levels):
-                    img = original_img.copy()
-                    img = rotate_and_sharpen_image(img, angle)
-                    img = resize_image(img, image_size)
-                    img = glitch(img, glitch_factor)
-
-                    base, ext = os.path.splitext(img_path)
-                    new_img_path = f"{base}_aug_angle{angle}_glitch{j}{ext}"
-                    print(f'Saving augmented image: {new_img_path}')
-                    img.save(new_img_path)
-
-print("All images augmented.")
+# Execute the pipeline
+p.sample(1000)  # Generate 1000 augmented images
--- testModel.py
+++ testModel.py
@@ -1,3 +1,4 @@
+import cv2
 from keras.models import load_model
 import numpy as np
 from PIL import Image
@@ -5,20 +6,39 @@ from PIL import Image
 # Load the trained model
 model = load_model("models/cnn_model/model.tf")
 
-# Open the image file
-img = Image.open('/Users/abdelkaderseifeddine/Documents/GitHub/CMC-7-trained-model/SVG_DATA/Images to test/77.png').convert('L')  # add convert('L') to make sure it's grayscale
+# Load the image
+image = cv2.imread("/Users/abdelkaderseifeddine/Documents/GitHub/CMC-7-trained-model/SVG_DATA/Images_to_test/3.png")
+image = cv2.fastNlMeansDenoisingColored(image,None,10,10,7,21)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Preprocess the image (resize, normalize, etc.)
-img = img.resize((64, 64))  # resize to 64x64
-img = np.array(img) / 255.0  # normalize
+# Threshold the image to separate characters
+res,thresh = cv2.threshold(gray,150,255,cv2.THRESH_BINARY)
 
-# Print the shape of img to debug
-print(img.shape)
+# Invert the image
+thresh = cv2.bitwise_not(thresh)
 
-img = img.reshape(1, 64, 64, 1)  # reshape to have batch size 1
+# Dilate the image
+kernel = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3))
+dilated = cv2.dilate(thresh, kernel, iterations=5)
 
-# Use the model to predict the class
-probs = model.predict(img)
-prediction = np.argmax(probs)
+# Find contours
+contours, hierarchy = cv2.findContours(dilated, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
 
-print(f"The predicted class is: {prediction}")
+# Filter contours based on size and sort
+coord = [cv2.boundingRect(contour) for contour in contours if 40 < cv2.boundingRect(contour)[2] < 300 and 40 < cv2.boundingRect(contour)[3] < 300]
+coord.sort(key=lambda tup: tup[0])  # sort by x coordinate
+
+# Predict each character
+for count, cor in enumerate(coord):
+    x, y, w, h = cor
+    char_image = gray[y:y+h, x:x+w]
+
+    # Preprocess the character image and use the model to predict
+    char_image = cv2.resize(char_image, (64, 64))
+    char_image = np.array(char_image) / 255.0  # normalize
+    char_image = char_image.reshape(1, 64, 64, 1)
+
+    # Predict the character
+    probs = model.predict(char_image)
+    prediction = np.argmax(probs)
+    print(f"The predicted class for character {count} is: {prediction}")
